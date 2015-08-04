e5a7f4772fc4dac0141cd1bb62a938a0285efc2a
Thanapat Bhunnachet <tgcruasde@gmail.com>
2015-08-04 14:32:57 +0700

    Step 3 - changing the configuration

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,15 @@
 // protractor.conf.js
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['spec.js'],
+    // capabilities: {
+    //     browserName: 'firefox'
+    // }
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }, /* {
+        browserName: 'PhantomJS'
+    } */]
 }
