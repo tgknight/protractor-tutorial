4a1f4d4d3eaf3e33d709a4712e7b3f65e6072a9e
Thanapat Bhunnachet <tgcruasde@gmail.com>
2015-08-04 14:20:21 +0700

    Step-1 - interacting with elements

--- spec.js
+++ spec.js
@@ -1,7 +1,38 @@
 // spec.js
 describe('Protractor Demo App', function() {
+
+    var SuperCalculator = function() {
+        var first = element(by.model('first'));
+        var second = element(by.model('second'));
+
+        this.get = function() {
+            browser.get('http://juliemr.github.io/protractor-demo');
+        }
+
+        this.setFirst = function(num) {
+            first.sendKeys(num);
+        }
+
+        this.setSecond = function(num) {
+            second.sendKeys(num);
+        }
+    }, superCalculator;
+
+    beforeEach(function() {
+        superCalculator = new SuperCalculator();
+    });
+
     it('should have a title', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+        superCalculator.get();
         expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    it('should add one and two', function() {
+        superCalculator.get();
+        superCalculator.setFirst(1);
+        superCalculator.setSecond(2);
+        element(by.id('gobutton')).click();
+
+        expect(element(by.binding('latest')).getText()).toEqual(5); // This is wrong!
     });
 });
